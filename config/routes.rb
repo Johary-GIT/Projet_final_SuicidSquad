61d9e7dd4c30449e7e0b4758c680ee610c77a65a
Johary-GIT <johariniarivorakoto@gmail.com>
2021-02-25 17:05:19 +0300

    adding Cards controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
  
+  get 'cards/show'
   resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :shops, only: [:index, :show]
 
   resources :order_items
+
+  resource :cards, only: [:show]
 
 
   root 'shops#index'

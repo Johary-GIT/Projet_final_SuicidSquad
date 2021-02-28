6dce9b171b6fb5d2e0399e576ed34590f0e37594
Johary-GIT <johariniarivorakoto@gmail.com>
2021-02-28 18:00:25 +0300

    remove

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   get 'cards/show'
   resources :products
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :shops, only: [:index, :show] 
   resources :order_items
   resource :cards, only: [:show]
